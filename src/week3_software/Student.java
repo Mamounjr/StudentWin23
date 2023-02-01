8a6648818165f8eb2a22a0f65943a23710fef661
Mamoun <diabmam@sheridancollege.ca>
2023-02-01 16:26:12 -0500

    Added a address

---
+++ src/week3_software/Student.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week3_software;
+
+/**
+ *
+ * @author Admin
+ */
+public class Student {
+    
+    private String address;
+    private String name;
+     private String address;
+    public Student(String name){
+        this.name = name;
+    }
+    
+    public String getName(){
+     
+        return name;
+    }
+    
+    public String getAddress(){
+        
+        return address;
+    }      
+}
